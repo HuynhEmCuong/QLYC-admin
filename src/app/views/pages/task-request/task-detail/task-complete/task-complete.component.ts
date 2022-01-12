edab080640207afaa20f7bff6c043ee925f9b8c8
HuynhEmCuong <huynhcuongem7597@gmail.com>
2022-01-12 14:54:15 +0700

    #update status finish complete task request

--- src/app/views/pages/task-request/task-detail/task-complete/task-complete.component.ts
+++ src/app/views/pages/task-request/task-detail/task-complete/task-complete.component.ts
@@ -13,4 +13,17 @@ export class TaskCompleteComponent implements OnInit {
   ngOnInit() {
   }
 
+  formatNameFile(fileName: string) {
+    if (fileName) {
+      let fileExtension = fileName.split('.').pop();
+      if (fileName.length > 20) {
+        fileName = fileName.slice(0, 20) + '....' + fileExtension;
+      }
+    }
+    return fileName;
+  }
+
+  cancelTask(){
+    
+  }
 }
